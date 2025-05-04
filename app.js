fe30bb52ee116d9e67247629270763a2680438ff
Francesco La Barba <87284271+Fralas@users.noreply.github.com>
2025-05-04 17:56:19 +0200

    Fix - translation error

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API funzionante!');
+  res.send('Working API!');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server attivo su http://localhost:${PORT}`);
+  console.log(`Server active on: http://localhost:${PORT}`);
 });
